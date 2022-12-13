baf8da2fa868709c36f5926871d54505cc1ff446
David Nowinsky <david@nowinsky.net>
2022-12-13 14:05:34 +0100

    Clean template

---
+++ ui/src/AppStore.tsx
@@ -0,0 +1,5 @@
+export function AppStore() {
+  return <div>
+    <h1>Apps</h1>
+  </div>
+}
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { AppStore } from './AppStore'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppStore />
   </React.StrictMode>,
 )
