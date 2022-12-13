f4b34e7427fa9bec17aefbaf447119b2f99421c1
David Nowinsky <david@nowinsky.net>
2022-12-13 14:29:14 +0100

    Query to get apps

--- ui/src/AppStore.tsx
+++ ui/src/AppStore.tsx
@@ -1,5 +1,38 @@
+import { useQuery, gql } from "@apollo/client";
+
+const LIST_APPS = gql`
+  query ListApps($tenantId: ID!) {
+    appsForTenant(tenantId: $tenantId) {
+      url
+      name
+      id
+    }
+  }
+`;
+
 export function AppStore() {
-  return <div>
-    <h1>Apps</h1>
-  </div>
+  const { loading, error, data } = useQuery(LIST_APPS, {
+    variables: { tenantId: "tenant-2" },
+  });
+
+  return (
+    <div>
+      <h1>Apps</h1>
+      {loading ? (
+        <>Loading...</>
+      ) : error ? (
+        <>Oh no! An error occurred</>
+      ) : (
+        <ul>
+          {data.appsForTenant.map(
+            (d: { id: string; name: string; url: string }) => (
+              <li key={d.id}>
+                <a href={d.url}>{d.name}</a>
+              </li>
+            )
+          )}
+        </ul>
+      )}
+    </div>
+  );
 }
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { client } from './api'
 import { AppStore } from './AppStore'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppStore />
+    <ApolloProvider client={client}>
+      <AppStore />
+    </ApolloProvider>
   </React.StrictMode>,
 )
