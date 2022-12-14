803618d3ec0add79793a7106d2dfec344266c214
David Nowinsky <david@nowinsky.net>
2022-12-14 17:15:32 +0100

    Query for dataset by id

--- services/datasets/src/datasets/data_source.py
+++ services/datasets/src/datasets/data_source.py
@@ -6,3 +6,9 @@ with open('./data/datasets.json') as f:
 
 def find_datasets_for_app_id(app_id: str) -> list[dict]:
     return [d for d in DATASETS if d.get('appId') == app_id]
+
+
+def find_dataset_by_id(id: str) -> dict | None:
+    for d in DATASETS:
+        if d.get('id') == id:
+            return d
--- services/datasets/src/datasets/schema.py
+++ services/datasets/src/datasets/schema.py
@@ -1,16 +1,29 @@
-from ariadne import QueryType, load_schema_from_path, convert_kwargs_to_snake_case, ObjectType
-from ariadne.contrib.federation import make_federated_schema
+from ariadne import QueryType, load_schema_from_path, convert_kwargs_to_snake_case
+from ariadne.contrib.federation import make_federated_schema, FederatedObjectType
 
-from datasets.data_source import find_datasets_for_app_id
+from datasets.data_source import find_datasets_for_app_id, find_dataset_by_id
 
 type_defs = load_schema_from_path('./schema.graphql')
 query = QueryType()
-datasets = ObjectType("Datasets")
+datasets = FederatedObjectType("Datasets")
+
 
 @query.field("appDatasets")
 @convert_kwargs_to_snake_case
 def resolve_app_datasets(*_, app_id):
     return find_datasets_for_app_id(app_id)
+
+
+@query.field("dataset")
+@convert_kwargs_to_snake_case
+def resolve_dataset(*_, id):
+    return find_dataset_by_id(id)
+
+
+@datasets.reference_resolver
+@convert_kwargs_to_snake_case
+def resolve_dataset_reference(*_, representation):
+    return find_dataset_by_id(representation.get('id'))
 
 
 schema = make_federated_schema(type_defs, query)
